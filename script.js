01708530b92148af562c5300b687e0dc433d0c8d
Julianacardoso1984 <julianacardosorodrigues2@gmail.com>
2025-02-27 22:28:10 -0300

    Correção na criação do elemento da lista na função addTask

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
-const li = document.createElement("li");
+
 
 function addTask() {
     const task = inputBox.value.trim();
@@ -8,7 +8,7 @@ function addTask() {
       alert("Please write down a task");
       return;
     }
-  
+    const li = document.createElement("li");
     li.innerHTML = `
       <label>
         <input type="checkbox">
